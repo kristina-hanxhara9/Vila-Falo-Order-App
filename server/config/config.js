352fd281c9032aa1272d24dc5a0517b5dbe41a64
kristina-hanxhara9~ <K.Hanxhara@liverpool.ac.uk>
2025-05-14 10:44:11 +0100

    env

--- server/config/config.js
+++ server/config/config.js
@@ -3,6 +3,6 @@ module.exports = {
   jwtSecret: process.env.JWT_SECRET || 'your_jwt_secret_key',
   // No expiration is defined here since we want tokens to never expire
   port: process.env.PORT || 5000,
-  mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/vila-falo'
+  mongoURI: process.env.MONGO_URI || 'mongodb+srv://kristinazhidro97:vilafalo@cluster0.7kzfmxk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 };
   
